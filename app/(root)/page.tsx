17853b3b65f636371691676cd68aa8a3d58df709
Anton <anton333mai@gmail.com>
2025-11-24 15:25:53 +0900

    tailwind theme in gloval.css

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,7 +1,9 @@
 import React from "react";
 
-const page = () => {
-  return <div>hello</div>;
-};
-
-export default page;
+export default function Home() {
+  return (
+    <div className="text-3xl text-blue center">
+      StoreMe - The only storage solution you need.
+    </div>
+  );
+}
