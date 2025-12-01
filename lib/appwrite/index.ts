62166ce514141dfb123ce8c1003fdd8c9d688311
Anton <anton333mai@gmail.com>
2025-12-01 15:46:03 +0900

    create session client and create admin client functions

--- lib/appwrite/index.ts
+++ lib/appwrite/index.ts
@@ -1,8 +1,46 @@
 import { appwriteConfig } from "./config";
-import { Client } from "node-appwrite";
+import { Client, Account, Databases, Storage, Avatars } from "node-appwrite";
+import { cookies } from "next/headers";
 
 export const createSessionClient = async () => {
   const client = new Client()
     .setEndpoint(appwriteConfig.endpointUrl)
     .setProject(appwriteConfig.projectId);
+
+  return {
+    get account() {
+      return new Account(client);
+    },
+    get databases() {
+      return new Databases(client);
+    },
+  };
+};
+
+export const createAdminClient = async () => {
+  const client = new Client()
+    .setEndpoint(appwriteConfig.endpointUrl)
+    .setProject(appwriteConfig.projectId)
+    .setKey(appwriteConfig.secretKey);
+
+  const session = (await cookies()).get("appwrite-sesion");
+
+  if (!session || !session.value) throw new Error("No session");
+
+  client.setSession(session.value);
+
+  return {
+    get account() {
+      return new Account(client);
+    },
+    get databases() {
+      return new Databases(client);
+    },
+    get storage() {
+      return new Storage(client);
+    },
+    get avatars() {
+      return new Avatars(client);
+    },
+  };
 };
