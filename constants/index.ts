abd9e67cdadd9e69480500f0c3501d67fa72aa2f
Anton <anton333mai@gmail.com>
2025-12-07 16:37:11 +0900

    sidebar nav links

---
+++ constants/index.ts
@@ -0,0 +1,7 @@
+export const navItems = [
+  { name: "Dashboard", icon: "/assets/icons/dashboard.svg", url: "/" },
+  { name: "Documents", icon: "/assets/icons/dashboard.svg", url: "/doc" },
+  { name: "Images", icon: "/assets/icons/dashboard.svg", url: "/img" },
+  { name: "Media", icon: "/assets/icons/dashboard.svg", url: "/media" },
+  { name: "Others", icon: "/assets/icons/dashboard.svg", url: "/others" },
+];
