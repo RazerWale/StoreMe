48d010bbbe0afc4db5d9d49fa8fa0d8d8deeaa79
Anton <anton333mai@gmail.com>
2025-12-08 10:56:34 +0900

    navigation itemes update

--- constants/index.ts
+++ constants/index.ts
@@ -1,7 +1,27 @@
 export const navItems = [
-  { name: "Dashboard", icon: "/assets/icons/dashboard.svg", url: "/" },
-  { name: "Documents", icon: "/assets/icons/dashboard.svg", url: "/doc" },
-  { name: "Images", icon: "/assets/icons/dashboard.svg", url: "/img" },
-  { name: "Media", icon: "/assets/icons/dashboard.svg", url: "/media" },
-  { name: "Others", icon: "/assets/icons/dashboard.svg", url: "/others" },
+  {
+    name: "Dashboard",
+    icon: "/assets/icons/dashboard.svg",
+    url: "/",
+  },
+  {
+    name: "Documents",
+    icon: "/assets/icons/documents.svg",
+    url: "/documents",
+  },
+  {
+    name: "Images",
+    icon: "/assets/icons/images.svg",
+    url: "/images",
+  },
+  {
+    name: "Media",
+    icon: "/assets/icons/video.svg",
+    url: "/media",
+  },
+  {
+    name: "Others",
+    icon: "/assets/icons/others.svg",
+    url: "/others",
+  },
 ];
